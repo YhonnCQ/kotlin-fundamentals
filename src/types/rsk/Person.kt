85d352c21879c18833aa60335181cd2f9cb20e34
YhonnCQ <yhonncq@gmail.com>
2020-04-14 20:01:19 -0500

    feat: Companion Objects
    Using the 'object' Keyword
    Extending Objects
    Companion Objects
    Using Companion Objects

--- src/types/rsk/Person.kt
+++ src/types/rsk/Person.kt
@@ -1,8 +1,18 @@
 package types.rsk
 
 fun main() {
-    val kevin = Student("Kevin", "Jones", 1,"some tutor")
+    val kevin = Student("Kevin", "Jones", 0, "some tutor")
     print(kevin.id)
+}
+
+class Program {
+    companion object {
+        @JvmStatic
+        fun main() {
+            val kevin = Student("Kevin", "Jones", 0, "some tutor")
+            print(kevin.id)
+        }
+    }
 }
 
 abstract class Person(var firstName: String, var lastName: String) {
@@ -11,11 +21,16 @@ abstract class Person(var firstName: String, var lastName: String) {
     abstract fun getAddress(): String
 }
 
-class Student(firstName: String, lastName: String, _id: Int, var tutor: String = ""): Person(firstName, lastName) {
+open class Student(firstName: String, lastName: String, _id: Int, var tutor: String = ""): Person(firstName, lastName) {
     val id: Int
 
     init {
         id = _id
+    }
+
+    fun enrole(courseName: String){
+        val course = Courses.allCourses
+            .firstOrNull { it.Title == courseName }
     }
 
     override fun getName(): String{return ""}
@@ -23,4 +38,30 @@ class Student(firstName: String, lastName: String, _id: Int, var tutor: String =
     override fun getAddress(): String {
         return ""
     }
+
+    companion object: XmlSerializer<Student> {
+        fun createUndergrad(name: String): Undergraduate {
+            return Undergraduate(name)
+        }
+
+        fun createPostgrad(name: String): Postgraduate {
+            return Postgraduate(name)
+        }
+
+        override fun toXml(item: Student) {
+
+        }
+    }
+}
+
+class Undergraduate(firstName: String): Student(firstName, "", 1) {
+
+}
+
+class Postgraduate(firstName: String): Student(firstName, "", 1) {
+
+}
+
+interface XmlSerializer<T> {
+    fun toXml(item: T)
 }
