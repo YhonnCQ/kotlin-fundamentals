199ed4d1dbd93251c2438433e6028eda0788220a
YhonnCQ <YhonnCQ@gmail.com>
2020-04-30 17:06:45 -0500

    feat: Higher Order Functions
    Declaring and Using Higher-order Functions
    Kotlin's Support for In-lining Functions

---
+++ src/higherorder/rsk/HigherOrder.kt
@@ -0,0 +1,12 @@
+package higherorder.rsk
+
+val action = { println("Hello, World") }
+val calc: (Int, Int) -> Int = { x, y -> x * y }
+
+fun main() {
+    doSomething(action)
+}
+
+fun doSomething(func: () -> Unit) {
+    func()
+}
