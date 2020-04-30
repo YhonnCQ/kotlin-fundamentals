6a20f68fbdec46a53ea222aad29aac6cbe23d7eb
YhonnCQ <YhonnCQ@gmail.com>
2020-04-29 19:42:37 -0500

    feat: Kotlin’s Nullablity Constraints
    Null Checking in Kotlin
    Using the 'let' Construct
    Using LateInit to Initialize Variables

---
+++ src/nullchecking/rsk/java/Organizer.java
@@ -0,0 +1,16 @@
+package nullchecking.rsk.java;
+
+public class Organizer {
+    boolean closeMeeting(Meeting meeting) {
+        if (meeting.canClose) return meeting.close();
+        return false;
+    }
+}
+
+class Meeting {
+    public boolean canClose;
+
+    public boolean close() {
+        return false;
+    }
+}
